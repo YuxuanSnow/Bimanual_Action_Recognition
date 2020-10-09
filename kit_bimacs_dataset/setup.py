02a892510a6f00d58596e1ca09a678ab3bd979e5
YuxuanSnow <yuxuan.xue@outlook.com>
2020-10-09 12:29:20 +0200

    comment on setup.py, how to publish module

--- kit_bimacs_dataset/setup.py
+++ kit_bimacs_dataset/setup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-#publish package 'bimacs'
+# distutils: provide developer convient way to publish new module
+# publish package 'bimacs'
+# to launch it: python setup.py sdist
+# automatically generated log file MANIFEST, generated module in disk folder named k_b_d-1.3.1.tar.gz
+# after installation of the module, user can import module and use the function
 
 setup(name='kit_bimacs_dataset',
       version='1.3.1',
